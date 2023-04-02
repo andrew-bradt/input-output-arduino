80dbc5ffa9855b1e9797c1e7cd77695060b8f4be
Andrew Bradt <andrewjbradt@gmail.com>
2023-04-02 19:51:13 -0400

    - initial max-msp code - config arduino to work with vs code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Tasks
-- 
+- Come up with Pseudocode for the Ultrasonic Censor, or learn how to use `SR04::Distance()`
--- uno/uno.ino
+++ uno/uno.ino
@@ -1,21 +1,4 @@
-//
-//
-//void setup(){
-//  Serial.begin(9600);
-//  Serial.println("uno.ino");
-//}
-//
-//void loop(){
-//  
-//}
-
-//www.elegoo.com
-//2016.12.08
 #include "/Users/andrewbradt/exhibition_may_2023/arduino/lib/ultrasound.h"
-//#define TRIG_PIN 2
-//#define ECHO_PIN 4
-//SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
-//long a;
 
 void setup() {
    Serial.begin(9600);
@@ -23,11 +6,5 @@ void setup() {
 }
 
 void loop() {
-//   a=sr04.Distance();
-//   if(a < 30) {
-//    Serial.print(a);
-//    Serial.println("cm"); 
-//   }
-//   delay(100);
-     Serial.println(getDistance());
+   Serial.println(getDistance());
 }
